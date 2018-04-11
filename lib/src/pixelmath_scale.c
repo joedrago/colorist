415c77e2f61b0a8f0ba6e218422e64fb3ba64234
Joe Drago <jdrago@netflix.com>
2018-04-11 16:09:35 -0700

    Fix very silly accidental grayscale with copypasta

--- lib/src/pixelmath_scale.c
+++ lib/src/pixelmath_scale.c
@@ -15,11 +15,11 @@ void clPixelMathScaleLuminance(float * pixels, int pixelCount, float luminanceSc
             pixel[1] *= luminanceScale;                     // scale
             pixel[1] = (pixel[1] < 0.0f) ? 0.0f : pixel[1]; // max(0, v)
             pixel[1] = pixel[1] / (1.0f + pixel[1]);        // reinhard tonemap
-            pixel[1] = CL_CLAMP(pixel[0], 0.0f, 1.0f);      // clamp
+            pixel[1] = CL_CLAMP(pixel[1], 0.0f, 1.0f);      // clamp
             pixel[2] *= luminanceScale;                     // scale
             pixel[2] = (pixel[2] < 0.0f) ? 0.0f : pixel[2]; // max(0, v)
             pixel[2] = pixel[2] / (1.0f + pixel[2]);        // reinhard tonemap
-            pixel[2] = CL_CLAMP(pixel[0], 0.0f, 1.0f);      // clamp
+            pixel[2] = CL_CLAMP(pixel[2], 0.0f, 1.0f);      // clamp
             pixel += 4;
         }
     } else {
@@ -28,9 +28,9 @@ void clPixelMathScaleLuminance(float * pixels, int pixelCount, float luminanceSc
             pixel[0] *= luminanceScale;                // scale
             pixel[0] = CL_CLAMP(pixel[0], 0.0f, 1.0f); // clamp
             pixel[1] *= luminanceScale;                // scale
-            pixel[1] = CL_CLAMP(pixel[0], 0.0f, 1.0f); // clamp
+            pixel[1] = CL_CLAMP(pixel[1], 0.0f, 1.0f); // clamp
             pixel[2] *= luminanceScale;                // scale
-            pixel[2] = CL_CLAMP(pixel[0], 0.0f, 1.0f); // clamp
+            pixel[2] = CL_CLAMP(pixel[1], 0.0f, 1.0f); // clamp
             pixel += 4;
         }
     }
