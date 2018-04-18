9b8ca9ca5e846a0f5c19f016ba39159f46f93a4a
Joe Drago <joedrago@gmail.com>
2018-04-17 21:03:22 -0700

    Add convenience files for local WYSIWYG docs writing

---
+++ Gemfile
@@ -0,0 +1,4 @@
+# This is just a convenience file for documentation editing.
+
+source 'https://rubygems.org'
+gem 'github-pages', group: :jekyll_plugins
---
+++ serve.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This is just a convenience file for documentation editing.
+
+# requires: sudo gem install bundle
+
+bundle install
+bundle exec jekyll serve
+
