1a1bf3121bfb83f29fe33f15437db5ec0714cda8
Joe Drago <jdrago@netflix.com>
2018-05-14 10:42:33 -0700

    Make image debugdump depth agnostic

--- lib/src/image_debugdump.c
+++ lib/src/image_debugdump.c
@@ -41,30 +41,28 @@ void clImageDebugDump(struct clContext * C, clImage * image, int x, int y, int w
 
 static void dumpPixel(struct clContext * C, clImage * image, cmsHTRANSFORM toXYZ, int x, int y, int extraIndent)
 {
-    COLORIST_ASSERT(image->pixels);
     int intRGB[4];
-    float maxChannel;
+    float maxChannel = (float)((1 << image->depth) - 1);
     float floatRGB[4];
     float floatXYZ[3];
     cmsCIEXYZ XYZ;
     cmsCIExyY xyY;
 
-    if (image->depth == 16) {
+    COLORIST_ASSERT(image->pixels);
+
+    if (image->depth > 8) {
         uint16_t * shorts = (uint16_t *)image->pixels;
         uint16_t * pixel = &shorts[4 * (x + (y * image->width))];
         intRGB[0] = pixel[0];
         intRGB[1] = pixel[1];
         intRGB[2] = pixel[2];
         intRGB[3] = pixel[3];
-        maxChannel = 65535.0f;
     } else {
         uint8_t * pixel = &image->pixels[4 * (x + (y * image->width))];
-        COLORIST_ASSERT(image->depth == 8);
         intRGB[0] = pixel[0];
         intRGB[1] = pixel[1];
         intRGB[2] = pixel[2];
         intRGB[3] = pixel[3];
-        maxChannel = 255.0f;
     }
 
     floatRGB[0] = intRGB[0] / maxChannel;
