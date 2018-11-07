bb8ebf394071f988899a82a64ef8ea30af40932c
Joe Drago <jdrago@netflix.com>
2018-11-07 15:59:20 -0800

    Switch exports in coverage.sh to be less explicit, for Travis

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -2,7 +2,7 @@
 
 mkdir ./build_coverage
 cd ./build_coverage
-export CC=/usr/bin/clang
-export CXX=/usr/bin/clang++
+export CC=clang
+export CXX=clang++
 cmake -G Ninja -DCOLORIST_COVERAGE=1 ..
 ninja colorist-test-ccov
