79bacc414417bb49f6f683cc7f03f5bdb56e70b1
Joe Drago <joedrago@gmail.com>
2019-02-07 00:35:11 -0800

    checkpoint number 2

--- lib/src/format_avif.c
+++ lib/src/format_avif.c
@@ -31,8 +31,7 @@ struct clImage * clFormatReadAVIF(struct clContext * C, const char * formatName,
     raw.data = input->ptr;
     raw.size = input->size;
 
-    avifImage * avif = avifImageCreate();
-    avifImage * rgba = avifImageCreate();
+    avifImage * avif = avifImageCreateEmpty();
     avifResult decodeResult = avifImageRead(avif, &raw);
     if ((decodeResult != AVIF_RESULT_OK) || !avif->width || !avif->height) {
         clContextLogError(C, "Failed to decode AVIF (%d)", (int)decodeResult);
@@ -49,35 +48,56 @@ struct clImage * clFormatReadAVIF(struct clContext * C, const char * formatName,
     }
 #endif
 
-    avifResult reformatResult = avifImageReformatPixels(avif, rgba, AVIF_PIXEL_FORMAT_RGBA, avif->depth);
-    if (reformatResult != AVIF_RESULT_OK) {
-        clContextLogError(C, "Failed to reformat AVIF to RGBA (%d)", (int)reformatResult);
-        goto readCleanup;
-    }
-
     image = clImageCreate(C, avif->width, avif->height, avif->depth, profile);
 
-    for (int j = 0; j < image->height; ++j) {
-        for (int i = 0; i < image->width; ++i) {
-            uint16_t * pixel = &image->pixels[CL_CHANNELS_PER_PIXEL * (i + (j * image->width))];
-            for (int plane = 0; plane < 4; ++plane) {
-                pixel[plane] = rgba->planes[plane][i + (j * rgba->strides[plane])];
+    avifBool usesU16 = avifImageUsesU16(avif);
+    int maxChannel = (1 << image->depth) - 1;
+    if (usesU16) {
+        for (int j = 0; j < image->height; ++j) {
+            for (int i = 0; i < image->width; ++i) {
+                uint16_t * pixel = &image->pixels[CL_CHANNELS_PER_PIXEL * (i + (j * image->width))];
+                pixel[0] = *((uint16_t *)&avif->rgbPlanes[AVIF_CHAN_R][(i * 2) + (j * avif->rgbRowBytes[AVIF_CHAN_R])]);
+                pixel[1] = *((uint16_t *)&avif->rgbPlanes[AVIF_CHAN_G][(i * 2) + (j * avif->rgbRowBytes[AVIF_CHAN_G])]);
+                pixel[2] = *((uint16_t *)&avif->rgbPlanes[AVIF_CHAN_B][(i * 2) + (j * avif->rgbRowBytes[AVIF_CHAN_B])]);
+                if (avif->alphaPlane) {
+                    pixel[3] = *((uint16_t *)&avif->alphaPlane[(i * 2) + (j * avif->alphaRowBytes)]);
+                } else {
+                    pixel[3] = (uint16_t)maxChannel;
+                }
+            }
+        }
+    } else {
+        for (int j = 0; j < image->height; ++j) {
+            for (int i = 0; i < image->width; ++i) {
+                uint16_t * pixel = &image->pixels[CL_CHANNELS_PER_PIXEL * (i + (j * image->width))];
+                pixel[0] = avif->rgbPlanes[AVIF_CHAN_R][i + (j * avif->rgbRowBytes[AVIF_CHAN_R])];
+                pixel[1] = avif->rgbPlanes[AVIF_CHAN_G][i + (j * avif->rgbRowBytes[AVIF_CHAN_G])];
+                pixel[2] = avif->rgbPlanes[AVIF_CHAN_B][i + (j * avif->rgbRowBytes[AVIF_CHAN_B])];
+                if (avif->alphaPlane) {
+                    pixel[3] = avif->alphaPlane[i + (j * avif->alphaRowBytes)];
+                } else {
+                    pixel[3] = (uint8_t)maxChannel;
+                }
             }
         }
     }
 
 readCleanup:
     avifImageDestroy(avif);
-    avifImageDestroy(rgba);
     if (profile) {
         clProfileDestroy(C, profile);
     }
+
     return image;
 }
 
 clBool clFormatWriteAVIF(struct clContext * C, struct clImage * image, const char * formatName, struct clRaw * output, struct clWriteParams * writeParams)
 {
+    COLORIST_UNUSED(C);
+    COLORIST_UNUSED(image);
     COLORIST_UNUSED(formatName);
+    COLORIST_UNUSED(output);
+    COLORIST_UNUSED(writeParams);
 
     clBool writeResult = clTrue;
 
@@ -90,15 +110,24 @@ clBool clFormatWriteAVIF(struct clContext * C, struct clImage * image, const cha
     }
 #endif
 
-    avifImage * avif = avifImageCreate();
-    avifImageCreatePixels(avif, AVIF_PIXEL_FORMAT_RGBA, image->width, image->height, image->depth);
+    avifImage * avif = avifImageCreate(image->width, image->height, image->depth, AVIF_PIXEL_FORMAT_YUV444);
+    avifImageAllocatePlanes(avif, AVIF_PLANES_RGB | AVIF_PLANES_A);
     avifRawData avifOutput = AVIF_RAW_DATA_EMPTY;
 
+    avifBool usesU16 = avifImageUsesU16(avif);
     for (int j = 0; j < image->height; ++j) {
         for (int i = 0; i < image->width; ++i) {
             uint16_t * pixel = &image->pixels[CL_CHANNELS_PER_PIXEL * (i + (j * image->width))];
-            for (int plane = 0; plane < 4; ++plane) {
-                avif->planes[plane][i + (j * avif->strides[plane])] = pixel[plane];
+            if (usesU16) {
+                *((uint16_t *)&avif->rgbPlanes[AVIF_CHAN_R][(i * 2) + (j * avif->rgbRowBytes[AVIF_CHAN_R])]) = pixel[0];
+                *((uint16_t *)&avif->rgbPlanes[AVIF_CHAN_G][(i * 2) + (j * avif->rgbRowBytes[AVIF_CHAN_G])]) = pixel[1];
+                *((uint16_t *)&avif->rgbPlanes[AVIF_CHAN_B][(i * 2) + (j * avif->rgbRowBytes[AVIF_CHAN_B])]) = pixel[2];
+                *((uint16_t *)&avif->alphaPlane[(i * 2) + (j * avif->alphaRowBytes)]) = pixel[3];
+            } else {
+                avif->rgbPlanes[AVIF_CHAN_R][i + (j * avif->rgbRowBytes[AVIF_CHAN_R])] = (uint8_t)pixel[0];
+                avif->rgbPlanes[AVIF_CHAN_G][i + (j * avif->rgbRowBytes[AVIF_CHAN_G])] = (uint8_t)pixel[1];
+                avif->rgbPlanes[AVIF_CHAN_B][i + (j * avif->rgbRowBytes[AVIF_CHAN_B])] = (uint8_t)pixel[2];
+                avif->alphaPlane[i + (j * avif->alphaRowBytes)] = (uint8_t)pixel[3];
             }
         }
     }
@@ -126,5 +155,6 @@ writeCleanup:
     }
     avifRawDataFree(&avifOutput);
     // clRawFree(C, &rawProfile);
+
     return writeResult;
 }
