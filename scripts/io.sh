b9cd9d340284f6a02d2a21c531604ff9698ec07c
Joe Drago <jdrago@netflix.com>
2019-05-09 19:21:39 -0700

    Switch IO test suite to release build

--- scripts/io.sh
+++ scripts/io.sh
@@ -4,5 +4,5 @@ mkdir ./build_io
 cd ./build_io
 export CC=clang
 export CXX=clang++
-cmake -G Ninja -DCOLORIST_TEST_IO=1 ..
-ninja colorist-test-io
+cmake -DCMAKE_BUILD_TYPE=Release -DCOLORIST_TEST_IO=1 ..
+make colorist-test-io
