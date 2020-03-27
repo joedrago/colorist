1346683d79881d325bddda7b5daf6c72093d45fe
Benbuck Nason <bnason@netflix.com>
2020-03-26 18:23:03 -0700

    Add tiff planar support (#28)

--- lib/src/format_tiff.c
+++ lib/src/format_tiff.c
@@ -133,6 +133,7 @@ struct clImage * clFormatReadTIFF(struct clContext * C, const char * formatName,
     int depth = 0;
     int iccLen = 0;
     int channelCount = 0;
+    uint16_t planarConfig = PLANARCONFIG_CONTIG;
     int orientation = ORIENTATION_TOPLEFT;
     int sampleFormat = SAMPLEFORMAT_UINT;
     uint8_t * iccBuf = NULL;
@@ -178,6 +179,12 @@ struct clImage * clFormatReadTIFF(struct clContext * C, const char * formatName,
     TIFFGetField(tiff, TIFFTAG_SAMPLESPERPIXEL, &channelCount);
     if ((channelCount != 1) && (channelCount != 3) && (channelCount != 4)) {
         clContextLogError(C, "unsupported channelCount(%d) from TIFF", channelCount);
+        goto readCleanup;
+    }
+
+    TIFFGetField(tiff, TIFFTAG_PLANARCONFIG, &planarConfig);
+    if ((planarConfig != PLANARCONFIG_CONTIG) && (planarConfig != PLANARCONFIG_SEPARATE)) {
+        clContextLogError(C, "unsupported planarConfig(%u) from TIFF", planarConfig);
         goto readCleanup;
     }
 
@@ -241,109 +248,205 @@ struct clImage * clFormatReadTIFF(struct clContext * C, const char * formatName,
         pixels = (uint8_t *)image->pixelsU16;
         rowBytes = image->width * CL_BYTES_PER_PIXEL(CL_PIXELFORMAT_U16);
     }
-    for (rowIndex = 0; rowIndex < image->height; ++rowIndex) {
-        uint8_t * pixelRow;
-        if (orientation == ORIENTATION_TOPLEFT) {
-            pixelRow = &pixels[rowIndex * rowBytes];
-        } else {
-            // ORIENTATION_BOTLEFT
-            pixelRow = &pixels[(image->height - 1 - rowIndex) * rowBytes];
+
+    if (planarConfig == PLANARCONFIG_CONTIG) {
+        for (rowIndex = 0; rowIndex < image->height; ++rowIndex) {
+            uint8_t * pixelRow;
+            if (orientation == ORIENTATION_TOPLEFT) {
+                pixelRow = &pixels[rowIndex * rowBytes];
+            } else {
+                // ORIENTATION_BOTLEFT
+                pixelRow = &pixels[(image->height - 1 - rowIndex) * rowBytes];
+            }
+            if (TIFFReadScanline(tiff, pixelRow, rowIndex, 0) < 0) {
+                clContextLogError(C, "Failed to read TIFF scanline row %d", rowIndex);
+                clImageDestroy(C, image);
+                image = NULL;
+                goto readCleanup;
+            }
+
+            if (channelCount == 1) {
+                // Expand grey in-place into RGBA, then fill A
+                if (fp32) {
+                    for (int x = image->width - 1; x >= 0; --x) {
+                        float * srcPixel = (float *)&pixelRow[x * sizeof(float)];
+                        float * dstPixel = (float *)&pixelRow[x * 4 * sizeof(float)];
+                        dstPixel[3] = 1.0f;
+                        dstPixel[2] = srcPixel[0];
+                        dstPixel[1] = srcPixel[0];
+                        dstPixel[0] = srcPixel[0];
+                    }
+                } else if (depth == 1) {
+                    int shift = 7 - (image->width % 8);
+                    for (int x = image->width - 1; x >= 0; --x) {
+                        uint8_t mask = (uint8_t)(1 << (7 - shift));
+                        uint8_t * srcPixel = &pixelRow[(x / 8) * sizeof(uint8_t)];
+                        uint8_t * dstPixel = &pixelRow[x * 4 * sizeof(uint8_t)];
+                        dstPixel[3] = 255;
+                        dstPixel[2] = (uint8_t)((srcPixel[0] & mask) << shift);
+                        dstPixel[1] = (uint8_t)((srcPixel[0] & mask) << shift);
+                        dstPixel[0] = (uint8_t)((srcPixel[0] & mask) << shift);
+                        --shift;
+                        if (shift < 0) {
+                            shift = 7;
+                        }
+                    }
+                } else if (depth == 8) {
+                    for (int x = image->width - 1; x >= 0; --x) {
+                        uint8_t * srcPixel = &pixelRow[x * sizeof(uint8_t)];
+                        uint8_t * dstPixel = &pixelRow[x * 4 * sizeof(uint8_t)];
+                        dstPixel[3] = 255;
+                        dstPixel[2] = srcPixel[0];
+                        dstPixel[1] = srcPixel[0];
+                        dstPixel[0] = srcPixel[0];
+                    }
+                } else {
+                    for (int x = image->width - 1; x >= 0; --x) {
+                        uint16_t * srcPixel = (uint16_t *)&pixelRow[x * sizeof(uint16_t)];
+                        uint16_t * dstPixel = (uint16_t *)&pixelRow[x * 4 * sizeof(uint16_t)];
+                        dstPixel[3] = 65535;
+                        dstPixel[2] = srcPixel[0];
+                        dstPixel[1] = srcPixel[0];
+                        dstPixel[0] = srcPixel[0];
+                    }
+                }
+            } else if (channelCount == 3) {
+                // Expand RGB in-place into RGBA, then fill A
+                if (fp32) {
+                    for (int x = image->width - 1; x >= 0; --x) {
+                        float * srcPixel = (float *)&pixelRow[x * 3 * sizeof(float)];
+                        float * dstPixel = (float *)&pixelRow[x * 4 * sizeof(float)];
+                        dstPixel[3] = 1.0f;
+                        dstPixel[2] = srcPixel[2];
+                        dstPixel[1] = srcPixel[1];
+                        dstPixel[0] = srcPixel[0];
+                    }
+                } else if (depth == 1) {
+                    int shift = 7 - (image->width % 8);
+                    for (int x = image->width - 1; x >= 0; --x) {
+                        uint8_t mask = (uint8_t)(1 << (7 - shift));
+                        uint8_t * srcPixel = &pixelRow[((x * 3) / 8) * sizeof(uint8_t)];
+                        uint8_t * dstPixel = &pixelRow[x * 4 * sizeof(uint8_t)];
+                        dstPixel[3] = 255;
+                        dstPixel[2] = (uint8_t)((srcPixel[2] & mask) << shift);
+                        dstPixel[1] = (uint8_t)((srcPixel[1] & mask) << shift);
+                        dstPixel[0] = (uint8_t)((srcPixel[0] & mask) << shift);
+                        --shift;
+                        if (shift < 0) {
+                            shift = 7;
+                        }
+                    }
+                } else if (depth == 8) {
+                    for (int x = image->width - 1; x >= 0; --x) {
+                        uint8_t * srcPixel = &pixelRow[x * 3 * sizeof(uint8_t)];
+                        uint8_t * dstPixel = &pixelRow[x * 4 * sizeof(uint8_t)];
+                        dstPixel[3] = 255;
+                        dstPixel[2] = srcPixel[2];
+                        dstPixel[1] = srcPixel[1];
+                        dstPixel[0] = srcPixel[0];
+                    }
+                } else {
+                    for (int x = image->width - 1; x >= 0; --x) {
+                        uint16_t * srcPixel = (uint16_t *)&pixelRow[x * 1 * sizeof(uint16_t)];
+                        uint16_t * dstPixel = (uint16_t *)&pixelRow[x * 4 * sizeof(uint16_t)];
+                        dstPixel[3] = 65535;
+                        dstPixel[2] = srcPixel[2];
+                        dstPixel[1] = srcPixel[1];
+                        dstPixel[0] = srcPixel[0];
+                    }
+                }
+            }
         }
-        if (TIFFReadScanline(tiff, pixelRow, rowIndex, 0) < 0) {
-            clContextLogError(C, "Failed to read TIFF scanline row %d", rowIndex);
-            clImageDestroy(C, image);
-            image = NULL;
+    } else if (planarConfig == PLANARCONFIG_SEPARATE) {
+        if (channelCount <= 1) {
+            clContextLogError(C, "unsupported planarConfig(%u) and channelCount(%d) from TIFF", planarConfig, channelCount);
             goto readCleanup;
         }
 
-        if (channelCount == 1) {
-            // Expand grey in-place into RGBA, then fill A
-            if (fp32) {
-                for (int x = image->width - 1; x >= 0; --x) {
-                    float * srcPixel = (float *)&pixelRow[x * sizeof(float)];
-                    float * dstPixel = (float *)&pixelRow[x * 4 * sizeof(float)];
-                    dstPixel[3] = 1.0f;
-                    dstPixel[2] = srcPixel[0];
-                    dstPixel[1] = srcPixel[0];
-                    dstPixel[0] = srcPixel[0];
+        uint8_t * readPixelRow = (uint8_t *)clAllocate(rowBytes);
+
+        for (int channel = 0; channel < channelCount; ++channel) {
+            for (rowIndex = 0; rowIndex < image->height; ++rowIndex) {
+                uint8_t * pixelRow;
+                if (orientation == ORIENTATION_TOPLEFT) {
+                    pixelRow = &pixels[rowIndex * rowBytes];
+                } else {
+                    // ORIENTATION_BOTLEFT
+                    pixelRow = &pixels[(image->height - 1 - rowIndex) * rowBytes];
                 }
-            } else if (depth == 1) {
-                int shift = 7 - (image->width % 8);
-                for (int x = image->width - 1; x >= 0; --x) {
-                    uint8_t mask = (uint8_t)(1 << (7 - shift));
-                    uint8_t * srcPixel = &pixelRow[(x / 8) * sizeof(uint8_t)];
-                    uint8_t * dstPixel = &pixelRow[x * 4 * sizeof(uint8_t)];
-                    dstPixel[3] = 255;
-                    dstPixel[2] = (uint8_t)((srcPixel[0] & mask) << shift);
-                    dstPixel[1] = (uint8_t)((srcPixel[0] & mask) << shift);
-                    dstPixel[0] = (uint8_t)((srcPixel[0] & mask) << shift);
-                    --shift;
-                    if (shift < 0) {
-                        shift = 7;
+                if (TIFFReadScanline(tiff, readPixelRow, rowIndex, (uint16_t)channel) < 0) {
+                    clContextLogError(C, "Failed to read TIFF scanline row %d", rowIndex);
+                    clImageDestroy(C, image);
+                    image = NULL;
+                    clFree(readPixelRow);
+                    goto readCleanup;
+                }
+
+                if (fp32) {
+                    float * srcPixel = (float *)readPixelRow;
+                    float * dstPixel = (float *)pixelRow;
+                    for (int x = 0; x < image->width; ++x) {
+                        dstPixel[channel] = srcPixel[0];
+                        dstPixel += 4;
+                        srcPixel += 1;
                     }
-                }
-            } else if (depth == 8) {
-                for (int x = image->width - 1; x >= 0; --x) {
-                    uint8_t * srcPixel = &pixelRow[x * sizeof(uint8_t)];
-                    uint8_t * dstPixel = &pixelRow[x * 4 * sizeof(uint8_t)];
-                    dstPixel[3] = 255;
-                    dstPixel[2] = srcPixel[0];
-                    dstPixel[1] = srcPixel[0];
-                    dstPixel[0] = srcPixel[0];
-                }
-            } else {
-                for (int x = image->width - 1; x >= 0; --x) {
-                    uint16_t * srcPixel = (uint16_t *)&pixelRow[x * sizeof(uint16_t)];
-                    uint16_t * dstPixel = (uint16_t *)&pixelRow[x * 4 * sizeof(uint16_t)];
-                    dstPixel[3] = 65535;
-                    dstPixel[2] = srcPixel[0];
-                    dstPixel[1] = srcPixel[0];
-                    dstPixel[0] = srcPixel[0];
+                } else if (depth == 1) {
+                    int shift = 7;
+                    uint8_t * srcPixel = (uint8_t *)readPixelRow;
+                    uint8_t * dstPixel = (uint8_t *)pixelRow;
+                    for (int x = 0; x < image->width; ++x) {
+                        uint8_t mask = (uint8_t)(1 << (7 - shift));
+                        dstPixel[channel] = (uint8_t)((srcPixel[0] & mask) << shift);
+                        dstPixel += 4;
+                        --shift;
+                        if (shift < 0) {
+                            shift = 7;
+                            srcPixel += 1;
+                        }
+                    }
+                } else if (depth == 8) {
+                    uint8_t * srcPixel = (uint8_t *)readPixelRow;
+                    uint8_t * dstPixel = (uint8_t *)pixelRow;
+                    for (int x = 0; x < image->width; ++x) {
+                        dstPixel[channel] = srcPixel[0];
+                    }
+                } else {
+                    uint16_t * srcPixel = (uint16_t *)readPixelRow;
+                    uint16_t * dstPixel = (uint16_t *)pixelRow;
+                    for (int x = 0; x < image->width; ++x) {
+                        dstPixel[channel] = srcPixel[0];
+                        dstPixel += 4;
+                        srcPixel += 1;
+                    }
                 }
             }
-        } else if (channelCount == 3) {
-            // Expand RGB in-place into RGBA, then fill A
-            if (fp32) {
-                for (int x = image->width - 1; x >= 0; --x) {
-                    float * srcPixel = (float *)&pixelRow[x * 3 * sizeof(float)];
-                    float * dstPixel = (float *)&pixelRow[x * 4 * sizeof(float)];
-                    dstPixel[3] = 1.0f;
-                    dstPixel[2] = srcPixel[2];
-                    dstPixel[1] = srcPixel[1];
-                    dstPixel[0] = srcPixel[0];
-                }
-            } else if (depth == 1) {
-                int shift = 7 - (image->width % 8);
-                for (int x = image->width - 1; x >= 0; --x) {
-                    uint8_t mask = (uint8_t)(1 << (7 - shift));
-                    uint8_t * srcPixel = &pixelRow[((x * 3) / 8) * sizeof(uint8_t)];
-                    uint8_t * dstPixel = &pixelRow[x * 4 * sizeof(uint8_t)];
-                    dstPixel[3] = 255;
-                    dstPixel[2] = (uint8_t)((srcPixel[2] & mask) << shift);
-                    dstPixel[1] = (uint8_t)((srcPixel[1] & mask) << shift);
-                    dstPixel[0] = (uint8_t)((srcPixel[0] & mask) << shift);
-                    --shift;
-                    if (shift < 0) {
-                        shift = 7;
+        }
+
+        clFree(readPixelRow);
+
+        // Fill A
+        if (channelCount == 3) {
+            for (rowIndex = 0; rowIndex < image->height; ++rowIndex) {
+                uint8_t * pixelRow = &pixels[rowIndex * rowBytes];
+
+                if (fp32) {
+                    float * dstPixel = (float *)pixelRow;
+                    for (int x = 0; x < image->width; ++x) {
+                        dstPixel[3] = 1.0f;
+                        dstPixel += 4;
                     }
-                }
-            } else if (depth == 8) {
-                for (int x = image->width - 1; x >= 0; --x) {
-                    uint8_t * srcPixel = &pixelRow[x * 3 * sizeof(uint8_t)];
-                    uint8_t * dstPixel = &pixelRow[x * 4 * sizeof(uint8_t)];
-                    dstPixel[3] = 255;
-                    dstPixel[2] = srcPixel[2];
-                    dstPixel[1] = srcPixel[1];
-                    dstPixel[0] = srcPixel[0];
-                }
-            } else {
-                for (int x = image->width - 1; x >= 0; --x) {
-                    uint16_t * srcPixel = (uint16_t *)&pixelRow[x * 3 * sizeof(uint16_t)];
-                    uint16_t * dstPixel = (uint16_t *)&pixelRow[x * 4 * sizeof(uint16_t)];
-                    dstPixel[3] = 65535;
-                    dstPixel[2] = srcPixel[2];
-                    dstPixel[1] = srcPixel[1];
-                    dstPixel[0] = srcPixel[0];
+                } else if ((depth == 1) || (depth == 8)) {
+                    uint8_t * dstPixel = pixelRow;
+                    for (int x = 0; x < image->width; ++x) {
+                        dstPixel[3] = 255;
+                        dstPixel += 4;
+                    }
+                } else {
+                    uint16_t * dstPixel = (uint16_t *)pixelRow;
+                    for (int x = 0; x < image->width; ++x) {
+                        dstPixel[3] = 65535;
+                        dstPixel += 4;
+                    }
                 }
             }
         }
