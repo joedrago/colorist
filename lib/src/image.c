2688eafb148dd5ba14c144f0ae38c282482d0cb0
Joe Drago <jdrago@netflix.com>
2020-09-22 12:45:11 -0700

    Revert "Clamp overranged (F32) RGB luminance when converting to U8 or U16"
    This reverts commit 37989b234f72f5da92491b9727a1d4392745b495.
    
    This is implicitly tonemapping overranged data when that is the responsibility of clTransform.

--- lib/src/image.c
+++ lib/src/image.c
@@ -103,13 +103,9 @@ void clImagePrepareReadPixels(struct clContext * C, clImage * image, clPixelForm
                         for (int i = 0; i < image->width; ++i) {
                             float * srcPixel = &image->pixelsF32[(i + (j * image->width)) * CL_CHANNELS_PER_PIXEL];
                             uint8_t * dstPixel = &image->pixelsU8[(i + (j * image->width)) * CL_CHANNELS_PER_PIXEL];
-                            float largestChannel = 1.0f;
-                            largestChannel = CL_MAX(largestChannel, srcPixel[0]);
-                            largestChannel = CL_MAX(largestChannel, srcPixel[1]);
-                            largestChannel = CL_MAX(largestChannel, srcPixel[2]);
-                            dstPixel[0] = (uint8_t)clPixelMathRoundUNorm(srcPixel[0] / largestChannel, maxChannelU8);
-                            dstPixel[1] = (uint8_t)clPixelMathRoundUNorm(srcPixel[1] / largestChannel, maxChannelU8);
-                            dstPixel[2] = (uint8_t)clPixelMathRoundUNorm(srcPixel[2] / largestChannel, maxChannelU8);
+                            dstPixel[0] = (uint8_t)clPixelMathRoundUNorm(srcPixel[0], maxChannelU8);
+                            dstPixel[1] = (uint8_t)clPixelMathRoundUNorm(srcPixel[1], maxChannelU8);
+                            dstPixel[2] = (uint8_t)clPixelMathRoundUNorm(srcPixel[2], maxChannelU8);
                             dstPixel[3] = (uint8_t)clPixelMathRoundUNorm(srcPixel[3], maxChannelU8);
                         }
                     }
@@ -142,13 +138,9 @@ void clImagePrepareReadPixels(struct clContext * C, clImage * image, clPixelForm
                         for (int i = 0; i < image->width; ++i) {
                             float * srcPixel = &image->pixelsF32[(i + (j * image->width)) * CL_CHANNELS_PER_PIXEL];
                             uint16_t * dstPixel = &image->pixelsU16[(i + (j * image->width)) * CL_CHANNELS_PER_PIXEL];
-                            float largestChannel = 1.0f;
-                            largestChannel = CL_MAX(largestChannel, srcPixel[0]);
-                            largestChannel = CL_MAX(largestChannel, srcPixel[1]);
-                            largestChannel = CL_MAX(largestChannel, srcPixel[2]);
-                            dstPixel[0] = (uint16_t)clPixelMathRoundUNorm(srcPixel[0] / largestChannel, maxChannelU16);
-                            dstPixel[1] = (uint16_t)clPixelMathRoundUNorm(srcPixel[1] / largestChannel, maxChannelU16);
-                            dstPixel[2] = (uint16_t)clPixelMathRoundUNorm(srcPixel[2] / largestChannel, maxChannelU16);
+                            dstPixel[0] = (uint16_t)clPixelMathRoundUNorm(srcPixel[0], maxChannelU16);
+                            dstPixel[1] = (uint16_t)clPixelMathRoundUNorm(srcPixel[1], maxChannelU16);
+                            dstPixel[2] = (uint16_t)clPixelMathRoundUNorm(srcPixel[2], maxChannelU16);
                             dstPixel[3] = (uint16_t)clPixelMathRoundUNorm(srcPixel[3], maxChannelU16);
                         }
                     }
